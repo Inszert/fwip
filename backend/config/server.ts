f72fe04ee6fb07554b6a4299d92042e2d2ccf8ae
Inszert <danielszabo.main@gmail.com>
2025-11-15 23:13:02 +0100

    asd

--- backend/config/server.ts
+++ backend/config/server.ts
@@ -1,10 +1,13 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('SERVER_URL', 'http://localhost:1337'),
+  url: env('SERVER_URL', 'https://tvoj-backend.onrender.com'),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'replace_me_with_random_secret'),
+      secret: env('ADMIN_JWT_SECRET', 'replace_with_random_string'),
     },
+  },
+  app: {
+    keys: env.array('APP_KEYS', ['key1', 'key2', 'key3', 'key4']),
   },
 });
