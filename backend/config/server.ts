80514b04d81d0989593fec39c35a8296b9631506
Inszert <danielszabo.main@gmail.com>
2025-11-15 23:01:30 +0100

    a

--- backend/config/server.ts
+++ backend/config/server.ts
@@ -1,7 +1,10 @@
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
+  url: env('SERVER_URL', 'http://localhost:1337'),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'replace_me_with_random_secret'),
+    },
   },
 });
