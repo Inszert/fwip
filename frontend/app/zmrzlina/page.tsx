5466870f1dc68b871026444230f73be87a4d4ee2
Daniel Szabo <danielszabo.main@gmail.com>
2026-01-07 17:28:12 +0100

    added chache to ice and video changed

--- frontend/app/zmrzlina/page.tsx
+++ frontend/app/zmrzlina/page.tsx
@@ -12,6 +12,9 @@ import {
   HeaderData,
   FooterData,
 } from "@/lib/strapi";
+// cahche enabled
+export const revalidate = 0; // Revalidate data every 60 seconds
+export const dynamic = "force-dynamic"; // Ensure the page is always rendered dynamically
 
 export default async function ZariadeniaPage() {
   let headerData: HeaderData = { button: [], image: null, subtitle: "" };
