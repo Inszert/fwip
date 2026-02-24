38e8877e6f99677d402f4ed6cf54064f6df99c63
Daniel Szabo <danielszabo.main@gmail.com>
2026-02-24 16:16:07 +0100

    f

--- frontend/next-sitemap.config.js
+++ frontend/next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://fwip.sk',
+  siteUrl: 'https://www.fwip.sk',
   generateRobotsTxt: true,
   changefreq: 'weekly',
   priority: 0.8,
